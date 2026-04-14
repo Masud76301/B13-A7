b923b845ff513580d83fba618e0525ca01b5b3fb
Masud76301 <khaledmasud76301@gmail.com>
2026-04-14 16:11:27 +0600

    Friends Details Layout Design Done

--- src/component/FriendsDetails/FriendsDetails.jsx
+++ src/component/FriendsDetails/FriendsDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router';
 import { RiNotificationSnoozeLine, RiDeleteBin6Line } from "react-icons/ri";
-import { LuArchive } from "react-icons/lu";
+import { LuArchive, LuVideo} from "react-icons/lu";
+import { FiPhoneCall } from "react-icons/fi";
+import { PiChatTextBold } from "react-icons/pi";
 
 const statusColor = {
     "overdue": "bg-red-600",
@@ -15,6 +17,8 @@ const FriendsDetails = () => {
     const expectedFriend = friends.find(friend => friend.id === Number(id));
     return (
         <div className='container mx-auto flex gap-6 my-20'>
+
+            {/* Left Column — Friend Info Card: */}
             <div className=''>
                 <div className='w-87 h-auto shadow-md flex flex-col justify-center items-center py-6 rounded-sm space-y-2'>
                     <img src={expectedFriend.picture} className='w-20 rounded-full' alt="" />
@@ -35,24 +39,65 @@ const FriendsDetails = () => {
                     <button className="btn bg-white w-87 flex gap-2 text-red-600"><RiDeleteBin6Line /> Delete</button>
                 </div>
             </div>
-            <div className=''>
+
+            {/* Right Column — 3 sections: */}
+            <div className=' flex flex-col gap-6'>
                 <div className='grid grid-cols-3 gap-4'>
                     <div className='w-58 h-auto p-8 shadow-sm rounded-sm text-center space-y-3'>
                         <h1 className='text-[28px] font-semibold'>{expectedFriend.days_since_contact}</h1>
                         <p className='text-[#64748B]'>Days since contact</p>
                     </div>
-                     <div className='w-58 h-auto p-8 shadow-sm rounded-sm text-center space-y-3'>
+                    <div className='w-58 h-auto p-8 shadow-sm rounded-sm text-center space-y-3'>
                         <h1 className='text-[28px] font-semibold'>{expectedFriend.goal}</h1>
                         <p className='text-[#64748B]'>Goals (Days)</p>
                     </div>
-                     <div className='w-58 h-auto p-8 shadow-sm rounded-sm text-center space-y-3'>
+                    <div className='w-58 h-auto p-8 shadow-sm rounded-sm text-center space-y-3'>
                         <h1 className='text-[28px] font-semibold'>{expectedFriend.next_due_date}</h1>
                         <p className='text-[#64748B]'>Next Due</p>
                     </div>
                 </div>
+
+                <div className='w-full  p-8 shadow-sm rounded-sm space-y-3 flex justify-between'>
+                    <div className='space-y-4' >
+                        <p className='text-[#244D3F] text-[20px] font-medium'>Relationship Goal</p>
+                        <h1 className='text-[18px] text-[#64748B]'>Connect every <span className='font-bold text-black'>{expectedFriend.goal} days</span></h1>
+                    </div>
+
+                    <button className='btn'>Edit</button>
+                </div>
+
+                <div className='w-full  p-6 shadow-sm rounded-sm space-y-3 '>
+                    <p>Quick Check-In</p>
+                    <div className='flex justify-between' >
+                        <div className='w-54 h-24 bg-[#F8FAFC] border border-[#E9E9E9] rounded-xl flex flex-col justify-center items-center '>
+
+                            <FiPhoneCall className='text-[32px]' />
+                            <p>Call</p>
+
+                        </div>
+                      
+                        <div className='w-54 h-24 bg-[#F8FAFC] border border-[#E9E9E9] rounded-xl flex flex-col justify-center items-center '>
+
+                            <PiChatTextBold className='text-[32px]' />
+                            <p>Text</p>
+
+                        </div>
+
+                          <div className='w-54 h-24 bg-[#F8FAFC] border border-[#E9E9E9] rounded-xl flex flex-col justify-center items-center '>
+
+                            <LuVideo className='text-[32px]' />
+                            <p>Video</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
             </div>
         </div>
-        
+
     );
 };
 
